c2b6ccb1afe91bcfa0a6c108d8ce71d62be9802a
dasaach <daryazdrav@yandex.ru>
2024-12-22 19:18:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
           prevEl: '.swiper-button-prev',
         },
         loop: true,
-        loopedSlides: 7, 
+        loopedSlides: 8, 
     });  
     const currentPage = document.querySelector('.current-page');
     const totalPages = document.querySelector('.total-pages');
